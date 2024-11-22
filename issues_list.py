394c6cc946b2a4733d000f71e772a65c2c6f9c52
micaelleos <micaelle.oliveira10@hotmail.com>
2024-11-22 09:07:50 -0300

    adding information card

--- issues_list.py
+++ issues_list.py
@@ -1,14 +1,20 @@
 # lista issues criados na sessão
 import streamlit as st
 
-with st.container():
 
-    for issue in st.session_state.issues:
-        with st.container(height=400, border=True):
-            st.write(f"**Key:** {issue['key']}")
-            st.write(f"**Título: {issue['summary']}**")
-            st.write(f"**Descrição**:{issue['description']}")
-            st.write("Mais informações:")
-            st.write(issue)
+if len(st.session_state.issues)==0:
+    st.info("Você ainda não criou nenhum issue.")
+
+else:
+
+    with st.container():
+
+        for issue in st.session_state.issues:
+            with st.container(height=400, border=True):
+                st.write(f"**Key:** {issue['key']}")
+                st.write(f"**Título: {issue['summary']}**")
+                st.write(f"**Descrição**:{issue['description']}")
+                st.write("Mais informações:")
+                st.write(issue)
 
     
